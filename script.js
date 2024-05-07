3e9925533d608cd6ccc1394bc0e47656448fb211
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2024-05-07 20:21:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,10 @@ async function renderPokemon(pokemon){
         pokemonImage.style.display = 'block'
         pokemonName.innerHTML = data.name
         pokemonNumber.innerHTML = data.id
+        //Imagem dos Pokemons com gif
         //pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+
+        //Imagem das artes oficiais dos Pokemons
         pokemonImage.src = data['sprites']['other']['official-artwork']['front_default']
         input.value = '';
         searchPokemon = data.id;
